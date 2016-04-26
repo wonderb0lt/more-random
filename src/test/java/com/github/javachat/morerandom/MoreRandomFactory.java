89890c5ca47e11fe36b497c88be73492e2ae53f1
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2016-04-26 15:50:04 +0200

    Removed unused fluff from tests

--- src/test/java/com/github/javachat/morerandom/MoreRandomFactory.java
+++ src/test/java/com/github/javachat/morerandom/MoreRandomFactory.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class MoreRandomFactory {
     private static int SEED = 4; // Chosen by fair dice roll
-    private static Random SEEDED_INSTANCE = new Random(SEED);
-    public static int RANDOM_INTS_TO_COLLECT = 15;
-    public static int[] EXPECTED_VALUES = SEEDED_INSTANCE.ints(RANDOM_INTS_TO_COLLECT).toArray();
 
     public static MoreRandom stable() {
         return new MoreRandom(new Random(SEED));
     }
+
+
 }
